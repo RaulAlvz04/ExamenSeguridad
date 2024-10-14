fcb88a51335c187cc59044a851c397e3d55ac3cd
Raul <rasty04ra@gmail.com>
2024-10-14 20:55:23 +0200

    Prueba commit con GPG

--- buscar_hash.sh
+++ buscar_hash.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Descomprimir el archivo imagenes.zip directamente en carpeta_imagenes
+
 echo "Descomprimiendo imagenes.zip..."
 unzip -q imagenes.zip -d carpeta_imagenes
 
-# Verificar si se descomprimió correctamente
+
 if [ $? -eq 0 ]; then
     echo "Archivo descomprimido en carpeta_imagenes."
 else
@@ -12,10 +12,10 @@ else
     exit 1
 fi
 
-# Hash MD5 que estamos buscando
+
 HASH_BUSCADO="e5ed313192776744b9b93b1320b5e268"
 
-# Buscar el archivo con el hash MD5 especificado
+
 echo "Buscando archivo con hash MD5 $HASH_BUSCADO..."
 for file in carpeta_imagenes/*; do
     HASH_ACTUAL=$(md5sum "$file" | awk '{print $1}')
